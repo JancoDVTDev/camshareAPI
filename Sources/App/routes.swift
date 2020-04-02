b79d69dc8257e7a17662063cf390542befa8cafa
Janco Erasmus <jerasmus@jhb.dvt.co.za>
2020-04-02 19:54:19 +0200

    Initial commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,6 +11,22 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+    // Validate Email
+    router.get("validate", String.parameter) { request -> String in
+        let email = try request.parameters.next(String.self)
+        var error = ""
+        // Check Email
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        let valid = emailPredicate.evaluate(with: email)
+        // Error Message
+        if !valid {
+            error = "Email format is not valid"
+        }
+        
+        return "Error Response: \(error)"
+    }
 
     // Example of configuring a controller
     let todoController = TodoController()
